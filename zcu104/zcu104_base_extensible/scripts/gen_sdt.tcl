4fc7c838ee2f51b3bd059654f067e08c8df6206c
Swagath Gadde <swagath.gadde@amd.com>
2024-06-06 14:08:30 +0530

    gen_sdt: add support to generate SDT files.
    Add the support to generate the SDT files after xsa generation.
    
    Signed-off-by: Swagath Gadde <swagath.gadde@amd.com>

---
+++ zcu104/zcu104_base_extensible/scripts/gen_sdt.tcl
@@ -0,0 +1,17 @@
+# parse arguments
+for { set i 0 } { $i < $argc } { incr i } {
+  # xsa name
+  if { [lindex $argv $i] == "-xsa_name" } {
+    incr i
+    set xsa_name [lindex $argv $i]
+  }
+}
+
+set xsa_path ./hw_project/$xsa_name/outputs/$xsa_name.xsa
+
+sdtgen set_dt_param -debug enable
+sdtgen set_dt_param -dir ./hw_project_sdt
+sdtgen set_dt_param -xsa $xsa_path
+sdtgen set_dt_param -board_dts zcu104-reva
+sdtgen generate_sdt
+sdtgen copy_hw_files
