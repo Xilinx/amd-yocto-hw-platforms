33a44d7b1833a270a51ebfed447ff897fe7e681f
Swagath Gadde <swagath.gadde@amd.com>
2024-06-28 12:52:13 +0530

    eval-brd-base:vek280: add vek280 base design support.
    Create eval-brd-base directory and add vek280_base design support with PS only enabled
    with segmented configuration.
    
    Signed-off-by: Swagath Gadde <swagath.gadde@amd.com>

---
+++ eval_brd_base/vek280_base/scripts/gen_sdt.tcl
@@ -0,0 +1,20 @@
+# Copyright (C) 2024, Advanced Micro Devices, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+# parse arguments
+for { set i 0 } { $i < $argc } { incr i } {
+  # xsa name
+  if { [lindex $argv $i] == "-xsa_name" } {
+    incr i
+    set xsa_name [lindex $argv $i]
+  }
+}
+
+set xsa_path ./hw_project/$xsa_name/outputs/$xsa_name.xsa
+
+sdtgen set_dt_param -debug enable
+sdtgen set_dt_param -dir ./hw_project_sdt
+sdtgen set_dt_param -xsa $xsa_path
+sdtgen set_dt_param -board_dts versal-vek280-revb
+sdtgen generate_sdt
+sdtgen copy_hw_files
