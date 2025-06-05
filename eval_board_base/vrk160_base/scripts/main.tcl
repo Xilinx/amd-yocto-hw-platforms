479da267b3e64ade0a6ebab0eba14ccd07e92fa9
Swagath Gadde <swagath.gadde@amd.com>
2025-06-05 12:04:06 +0530

    main.tcl: remove vrk160 workaround for 2025.2
    Signed-off-by: Swagath Gadde <swagath.gadde@amd.com>

--- eval_board_base/vrk160_base/scripts/main.tcl
+++ eval_board_base/vrk160_base/scripts/main.tcl
@@ -23,9 +23,6 @@ for { set i 0 } { $i < $argc } { incr i } {
   }
  }
 
-#this is workaround for 2025.1, once we move to 2025.2 we can remove this line
-set_param board.repoPaths /proj/xbuilds/2025.2_daily_latest/installs/lin64/HEAD/data/xhub/boards/XilinxBoardStore/boards/Xilinx/vrk160/
-
 create_project $proj_name $proj_dir/$proj_name -part xcvr1602-vsva2488-2MP-e-S-es1
 set_property board_part xilinx.com:$board:part0:* [current_project]
 create_bd_design "versal_gen1_platform" -mode batch
