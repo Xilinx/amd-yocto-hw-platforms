e5c76a4c5ca721d5a3f5d7c13be979da588f730a
Swagath Gadde <swagath.gadde@amd.com>
2025-06-06 12:34:21 +0530

    main.tcl: update ced version.
    Since 2025.1 released ced has 1.0 version so updating the version.
    
    Signed-off-by: Swagath Gadde <swagath.gadde@amd.com>

--- eval_board_base/vek385_base/scripts/main.tcl
+++ eval_board_base/vek385_base/scripts/main.tcl
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 xhub::refresh_catalog [xhub::get_xstores Vivado_example_project]
-xhub::update [xhub::get_xitems xilinx.com:Vivado_example_project:versal_gen2_platform:1.1]
+xhub::update [xhub::get_xitems xilinx.com:Vivado_example_project:versal_gen2_platform:1.0]
 
 set proj_name project_1
 set proj_dir ./hw_project
