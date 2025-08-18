6afff93ae5b4d2f101a7774537b06b2c35738ab5
Swagath Gadde <swagath.gadde@amd.com>
2025-08-18 18:28:36 +0530

    vek385_revb_base:add vek385 revB design support.
    Add vek385 revB design build support using board files.
    
    Signed-off-by: Swagath Gadde <swagath.gadde@amd.com>

---
+++ eval_board_base/vek385_revb_base/scripts/gen_sdt.tcl
@@ -0,0 +1,20 @@
+# Copyright (C) 2024, Advanced Micro Devices, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+# parse arguments
+for { set i 0 } { $i < $argc } { incr i } {
+  # xsa name
+  if { [lindex $argv $i] == "-xsa_name" } {
+    incr i
+    set xsa_name [lindex $argv $i]
+  }
+}
+
+set xsa_path ./hw_project/$xsa_name/outputs/$xsa_name.xsa
+
+set_dt_param -debug enable
+set_dt_param -zocl enable
+set_dt_param -dir ./hw_project_sdt
+set_dt_param -xsa $xsa_path
+set_dt_param -board_dts versal2-vek385-reva
+generate_sdt
