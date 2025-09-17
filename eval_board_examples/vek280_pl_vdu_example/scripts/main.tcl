54675e45fab62b49f1034e7bbbbb3ffc9965a863
Swagath Gadde <swagath.gadde@amd.com>
2025-09-17 11:00:40 +0000

    vek280_pl: update to latest ced template
    since versal_gen1 ced template got removed from TA, so update to versal_comm ced.
    
    Signed-off-by: Swagath Gadde <swagath.gadde@amd.com>

--- eval_board_examples/vek280_pl_vdu_example/scripts/main.tcl
+++ eval_board_examples/vek280_pl_vdu_example/scripts/main.tcl
@@ -24,8 +24,8 @@ for { set i 0 } { $i < $argc } { incr i } {
 	        
 create_project $proj_name $proj_dir/$proj_name -part xcve2802-vsvh1760-2MP-e-S
 set_property board_part xilinx.com:$board:part0:* [current_project]
-create_bd_design "versal_gen1_platform" -mode batch
-instantiate_example_design -template xilinx.com:design:versal_gen1_platform:1.1 -design versal_gen1_platform -options { Design_type.VALUE Extensible}
+create_bd_design "versal_comn_platform" -mode batch
+instantiate_example_design -template xilinx.com:design:versal_comn_platform:2.0 -design versal_comn_platform -options { Design_type.VALUE Extensible}
 update_compile_order -fileset sources_1
 
 #source pl vdu tcl
