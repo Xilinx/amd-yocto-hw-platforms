be4702eef584a856fb6571aefbaf913cca72a5fe
Swagath Gadde <swagath.gadde@amd.com>
2025-09-18 22:13:20 +0530

    ve-p-a1225-00-psxc-seio: add x-prc-11-revA dts
    Since ve-p-a1225-00 char board has x-prc-11-revA daughter card connected
    so add x-prc-11-revA dts as custom dts in sdt.
    
    Signed-off-by: Swagath Gadde <swagath.gadde@amd.com>

--- eval_board_base/ve-p-a1225-00-psxc-seio/scripts/gen_sdt.tcl
+++ eval_board_base/ve-p-a1225-00-psxc-seio/scripts/gen_sdt.tcl
@@ -16,4 +16,5 @@ set_dt_param -zocl enable
 set_dt_param -dir ./hw_project_sdt
 set_dt_param -xsa $xsa_path
 set_dt_param -board_dts versal2-ve-p-a1225-00-reva
+set_dt_param -user_dts ./dts/x-prc-11-revA.dtsi
 generate_sdt
