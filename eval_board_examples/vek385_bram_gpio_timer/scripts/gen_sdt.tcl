168f9cdddd547d6b0ab803b77f0eeeee23fca06c
Swagath Gadde <swagath.gadde@amd.com>
2025-10-22 12:32:33 +0530

    vek385-pl-bram-gpio-timer-fw: update board dtsi name
    since we have versal2-vek385-reva so removed versal2-vek385-revA in sdtgen
    so update the board dtsi name accordingly.
    
    Signed-off-by: Swagath Gadde <swagath.gadde@amd.com>
    Acked-by: Raju Kumar Pothuraju <rajukumar.pothuraju@amd.com>

--- eval_board_examples/vek385_bram_gpio_timer/scripts/gen_sdt.tcl
+++ eval_board_examples/vek385_bram_gpio_timer/scripts/gen_sdt.tcl
@@ -16,5 +16,5 @@ sdtgen set_dt_param -debug enable
 sdtgen set_dt_param -zocl enable
 sdtgen set_dt_param -dir ./hw_project_sdt
 sdtgen set_dt_param -xsa $xsa_path
-sdtgen set_dt_param -board_dts versal2-vek385-revA
+sdtgen set_dt_param -board_dts versal2-vek385-reva
 sdtgen generate_sdt
