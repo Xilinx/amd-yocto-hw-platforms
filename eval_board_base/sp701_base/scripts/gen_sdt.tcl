ed4a73aa4735a7e24f510ed6a13f9ced99352363
PremSai Addepalli <PremSai.Addepalli@amd.com>
2025-11-26 15:20:02 +0530

    gen_sdt:Remove unwanted sdtgen call
    Remove unwanted sdtgen call
    
    Signed-off-by: PremSai Addepalli <PremSai.Addepalli@amd.com>
    
    Acked-by : Ashwini Lomate <ashwini.lomate@amd.com>

--- eval_board_base/sp701_base/scripts/gen_sdt.tcl
+++ eval_board_base/sp701_base/scripts/gen_sdt.tcl
@@ -15,5 +15,5 @@ set xsa_path ./hw_project/$xsa_name/outputs/$xsa_name.xsa
 set_dt_param -debug enable
 set_dt_param -dir ./hw_project_sdt
 set_dt_param -xsa $xsa_path
-sdtgen set_dt_param -board_dts sp701-rev1.0
+set_dt_param -board_dts sp701-rev1.0
 generate_sdt
