3d0eed4fb0bd6f6f4d8c01994ef51de65e358dfd
PremSai Addepalli <PremSai.Addepalli@amd.com>
2026-02-03 17:17:10 +0530

    sp701_base: add xilinx-mbv64-sp701.dts
    since compatible string is missing in sdt add compatible
    property as workaround in user-dts to resolve GMC build errors.
    
    Signed-off-by: PremSai Addepalli <PremSai.Addepalli@amd.com>
    Acked-by: Swagath Gadde <swagath.gadde@amd.com>

--- eval_board_base/sp701_base/scripts/gen_sdt.tcl
+++ eval_board_base/sp701_base/scripts/gen_sdt.tcl
@@ -16,4 +16,5 @@ set_dt_param -debug enable
 set_dt_param -dir ./hw_project_sdt
 set_dt_param -xsa $xsa_path
 set_dt_param -board_dts sp701-rev1.0
+set_dt_param -user_dts ./dts/xilinx-mbv64-sp701.dtsi
 generate_sdt
