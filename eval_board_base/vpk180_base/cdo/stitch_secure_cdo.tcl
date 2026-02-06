f9eaa782602e73f9b0c3f9dfed20df9b6f9f98bc
Ashwini Lomate <ashwini.lomate@amd.com>
2026-02-06 19:22:58 +0530

    Makefile:gen_sdt.tcl:main.tcl: Add vpk180 ced support
    Add CED design support for the vpk180_base configuration, which includes PS.
    And enable support for SDT generation.
    Add secure sysmon cdo support.
    
    Signed-off-by: Ashwini Lomate <ashwini.lomate@amd.com>

---
+++ eval_board_base/vpk180_base/cdo/stitch_secure_cdo.tcl
@@ -0,0 +1,17 @@
+# Copy the sysmon secure CDO to hw project root (if present)
+
+puts "Executing POST device image TCL"
+if { [file exists ../../../../cdo/secio-sysmon.v4.cdo] == 1 } {
+   puts "Copying Sysmon Secure CDO to Project"
+   file copy -force ../../../../cdo/secio-sysmon.v4.cdo ../../.
+}
+
+# Apply overlay CDO for secure sysmon usecase (if present)
+if { [file exists ../../secio-sysmon.v4.cdo] == 1 } {
+    puts "######### Regenerating PDI with overlay CDO applied #########"
+    puts "CWD '[pwd]'"
+    puts "Running '[exec which bootgen] -arch versal -image edf_base_wrapper_boot.bif -w -o ./edf_base_wrapper_boot.pdi -overlay_cdo ./hw_project/secio-sysmon.v4.cdo'"
+    exec mv ./edf_base_wrapper_boot.pdi ./edf_base_wrapper_boot.pdi.orig
+    exec [exec which bootgen] -arch versal -image edf_base_wrapper_boot.bif -w -o ./edf_base_wrapper_boot.pdi -overlay_cdo ../../secio-sysmon.v4.cdo
+}
+
