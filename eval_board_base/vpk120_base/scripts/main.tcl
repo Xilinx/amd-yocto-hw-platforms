82773a2c82256abb58b0f48954dfc3df3c637986
nithishk <nithishkumar.naroju@amd.com>
2026-03-03 17:10:35 +0530

    main.tcl: Update to new EDF Base Design for vpk120
    Update vpk120 board base design to incorporate
    new EDF Base Design with minimal pl payload default
    Acked-by: Swagath Gadde <swagath.gadde@amd.com>

--- eval_board_base/vpk120_base/scripts/main.tcl
+++ eval_board_base/vpk120_base/scripts/main.tcl
@@ -24,8 +24,8 @@ for { set i 0 } { $i < $argc } { incr i } {
 
 create_project $proj_name $proj_dir/$proj_name -part xcvp1202-vsva2785-2MP-e-S
 set_property board_part xilinx.com:vpk120:part0:* [current_project]
-create_bd_design "edf_base" -mode batch
-instantiate_example_design -template xilinx.com:design:edf_base:1.0 -design edf_base
+create_bd_design "edf_base_pl" -mode batch
+instantiate_example_design -template xilinx.com:design:edf_base_pl:1.0 -design edf_base_pl
 set_property segmented_configuration true [current_project]
 
 update_compile_order -fileset sources_1
