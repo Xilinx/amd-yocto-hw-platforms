50ba8c4cf31176fd7ec33800e68f9ab54c425af1
Dhananjay Chauhan <dhananjay.chauhan@amd.com>
2026-08-21 06:24:45 -0700

    vek280_extensible_platform: Generate CED platform with hw_emu XSA and NOC solution
    Replace the base-XSA copy flow with direct extensible CED platform
    generation from main.tcl. Add a NOC_SOLUTION make argument to pass a
    NOC solution .ncr file into the Vivado build, and generate a hw_emu XSA
    alongside the hardware XSA. Remove the now-unused enable_segmented_config.sh.
    
    Signed-off-by: Dhananjay Chauhan <dhananjay.chauhan@amd.com>
    Acked-by: Swagath Gadde <swagath.gadde@amd.com>

--- eval_board_examples/vek280_extensible_platform/scripts/main.tcl
+++ eval_board_examples/vek280_extensible_platform/scripts/main.tcl
@@ -7,6 +7,10 @@ set proj_name project_1
 set proj_dir ./hw_project
 set output_dir outputs
 set board vek280
+variable design_name
+set design_name vitis_design
+
+set noc_solution ""
 
 # parse arguments
 for { set i 0 } { $i < $argc } { incr i } {
@@ -20,21 +24,38 @@ for { set i 0 } { $i < $argc } { incr i } {
     incr i
     set jobs [lindex $argv $i]
   }
+  # noc solution file path
+  if { [lindex $argv $i] == "-noc_solution" } {
+    incr i
+    set noc_solution [lindex $argv $i]
+  }
  }
-	        
+
 create_project $proj_name $proj_dir/$proj_name -part xcve2802-vsvh1760-2MP-e-S
 set_property board_part xilinx.com:$board:part0:* [current_project]
-create_bd_design "versal_comn_platform" -mode batch
-instantiate_example_design -template xilinx.com:design:versal_comn_platform:* -design versal_comn_platform -options { Design_type.VALUE Extensible}
+set_property segmented_configuration true [current_project]
+
+# Emulation (hw_emu) prerequisite: Versal requires the TLM sim model.
+# Simulator defaults to XSim (Vivado built-in); no target_simulator override needed.
+set_property PREFERRED_SIM_MODEL "tlm" [current_project]
+
+create_bd_design $design_name -mode batch
+instantiate_example_design -template xilinx.com:design:base_ext_platform:1.0 -design $design_name
+
 update_compile_order -fileset sources_1
-        
+
 save_bd_design
+if { $noc_solution ne "" } {
+  puts "INFO: Reading NOC solution from: $noc_solution"
+  read_noc_solution $noc_solution
+}
+
 validate_bd_design
 file mkdir $proj_dir/$proj_name/$output_dir
 
 set outputs_dir $proj_dir/$proj_name/$output_dir
 
-set fd [open $outputs_dir/README.hw w] 
+set fd [open $outputs_dir/README.hw w]
 
 puts $fd "##########################################################################"
 puts $fd "This is a brief document containing design specific details for : ${board}"
@@ -43,11 +64,11 @@ puts $fd "######################################################################
 
 set board_part [get_board_parts [current_board_part -quiet]]
 if { $board_part != ""} {
-	puts $fd "BOARD: $board_part" 
+	puts $fd "BOARD: $board_part"
 }
 
 set design_name [get_property NAME [get_bd_designs]]
-puts $fd "BLOCK DESIGN: $design_name" 
+puts $fd "BLOCK DESIGN: $design_name"
 
 
 set columns {%40s%30s%15s%50s}
@@ -56,17 +77,32 @@ puts $fd [format $columns "MODULE INSTANCE NAME" "IP TYPE" "IP VERSION" "IP"]
 puts $fd [string repeat - 150]
 
 foreach ip [get_ips] {
-	set catlg_ip [get_ipdefs -all [get_property IPDEF $ip]]	
+	set catlg_ip [get_ipdefs -all [get_property IPDEF $ip]]
 	puts $fd [format $columns [get_property NAME $ip] [get_property NAME $catlg_ip] [get_property VERSION $catlg_ip] [get_property VLNV $catlg_ip]]
 }
 
 close $fd
 
+# ---- Emulation (hw_emu) XSA ----
+# Generated before synthesis: the hw_emu platform only needs the block design
+# simulation model, not implementation. Mirrors the vek280_base flow.
+generate_target all [get_files -norecurse ${design_name}.bd]
+update_compile_order -fileset sources_1
+# If the NoC switch network (xlnoc.bd) is not auto-generated for sim, uncomment:
+#generate_switch_network_for_noc
+update_compile_order -fileset sim_1
+launch_simulation -scripts_only
+launch_simulation -step compile
+launch_simulation -step elaborate
+file mkdir $outputs_dir/hw_emu
+write_hw_platform -hw_emu -force $outputs_dir/hw_emu/${proj_name}_hw_emu.xsa
+validate_hw_platform -verbose $outputs_dir/hw_emu/${proj_name}_hw_emu.xsa
+
 launch_runs synth_1 -jobs $jobs
 wait_on_run synth_1
-    
+
 launch_runs impl_1 -to_step write_bitstream
-            
+
 wait_on_run impl_1
 
 open_run impl_1
