c4488ff73e2279eed1da57fde2bb43791807f185
Konduru Sandeep Raju <SandeepRaju.Konduru@amd.com>
2026-09-10 12:26:37 +0530

    vek280_base: Fix NoC solution export ordering
    Open the impl_1 run before querying and locking the initial boot NoC
    routes. Previously, the route query and NoC solution export were
    performed before the implemented design was opened, potentially
    producing an incomplete or inconsistent NCR file.
    
    Use an explicit initial_boot filter expression to match the established
    extensible-platform flow.
    
    This prevents downstream implementations from resolving the exported
    NoC configuration differently.
    
    Signed-off-by: Konduru Sandeep Raju <SandeepRaju.Konduru@amd.com>
    
    Acked-by : Ashwini Lomate <ashwini.lomate@amd.com>

--- eval_board_base/vek280_base/scripts/main.tcl
+++ eval_board_base/vek280_base/scripts/main.tcl
@@ -71,9 +71,11 @@ launch_runs impl_1 -to_step write_bitstream
 
 wait_on_run impl_1
 
-set_property lock true [get_noc_net_routes -of [get_noc_logical_path -filter initial_boot]]
+open_run impl_1
+
+set_property lock true [get_noc_net_routes -of [get_noc_logical_path -filter {initial_boot == 1}]]
+
 write_noc_solution -file $outputs_dir/${design_name}_noc_solution.ncr
 
-open_run impl_1
 write_hw_platform -fixed -include_bit -file $outputs_dir/${proj_name}.xsa
 validate_hw_platform -verbose $outputs_dir/${proj_name}.xsa
